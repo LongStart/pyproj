db56e1d9c45c4a6717b83c3f12b43be82bc8ada3
XiaoxingChen <cxx@seer-robotics.com>
2017-12-08 09:56:53 +0800

    Modify: add serial test

---
+++ serial/showallserialports.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*
+import os
+try:
+    import serial
+except AttributeError:
+    os.system("pip install serial")
+import serial.tools.list_ports
+
+plist = list(serial.tools.list_ports.comports())
+
+if len(plist) <= 0:
+    print ("The Serial port can't find!")
+else:
+    plist_0 =list(plist[0])
+    serialName = plist_0[0]
+    serialFd = serial.Serial(serialName,9600,timeout = 60)
+    print ("check which port was really used >",serialFd.name)
