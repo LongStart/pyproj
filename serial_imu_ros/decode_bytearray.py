7c42513470843c9afd6454f4d23320a36c735e2d
XiaoxingChen <cxx@seer-robotics.com>
2018-11-23 15:12:58 +0800

    Modify: get data ok

--- serial_imu_ros/decode_bytearray.py
+++ serial_imu_ros/decode_bytearray.py
@@ -4,13 +4,22 @@ def decode_raw_adc_data(data_field):
     raw_adc_format_string = '>9h'
     mid_data = struct.unpack(raw_adc_format_string, data_field)
     return mid_data
+
+def strange_convert(raw_value):
+    temp = 0
+    if(raw_value & 0x8000):
+        temp = 0 - (raw_value & 0x7fff)
+    else:
+        temp = (raw_value & 0x7fff)
+    return temp / 10.
     
 def decode_euler_angle(data_field):
     euler_angle_format_string = '>7h'
     mid_data = struct.unpack(euler_angle_format_string, data_field)
-    yaw = mid_data[0]
-    pitch = mid_data[1]
-    roll = mid_data[2]
+
+    yaw = strange_convert(mid_data[0])
+    pitch = strange_convert(mid_data[1])
+    roll = strange_convert(mid_data[2])
     return (yaw, pitch, roll)
 
 def imu_check_sum(target_msg):
@@ -35,7 +44,8 @@ def decode_from_buffer(raw_msg):
 
     if(raw_msg[1] == adc_frame_type_id):
         return decode_raw_adc_data(raw_msg[2:-2])
-    elif(raw_msg[1] == adc_frame_type_id):
+    elif(raw_msg[1] == rpy_frame_type_id):
+        # print([v for v in raw_msg])
         return decode_euler_angle(raw_msg[2:-2])
     else:
         return None
--- serial_imu_ros/demo_v1.py
+++ serial_imu_ros/demo_v1.py
@@ -4,6 +4,7 @@ import sys
 import time
 import os
 import threading
+from decode_bytearray import *
 
 
 
@@ -19,6 +20,13 @@ buffer = bytearray(b'')
 
 while(True):
     cmd = b''
+    buff_len = len(buffer)
+    if(buff_len > 5 and 0xa5 == buffer[0] and 0x5a == buffer[1] and buff_len >= (buffer[2] + 2) ):
+        pack = decode_from_buffer(buffer[2:2+buffer[2]])
+        buffer = buffer[buffer[2] + 2:]
+        if(3 == len(pack)):
+            print(pack)
+
     try:
         cmd = com.read(1)
     except KeyboardInterrupt:
