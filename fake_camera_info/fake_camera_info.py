34b939368c4479fc4075dec5776fc5c89f15d2fc
XiaoxingChen <chen-xx@zju.edu.cn>
2019-05-10 18:42:11 +0800

    Add: fake camera info

---
+++ fake_camera_info/fake_camera_info.py
@@ -0,0 +1,39 @@
+import rospy
+import genpy
+import yaml
+from sensor_msgs.msg import CameraInfo
+from sensor_msgs.msg import Image
+from sys import argv
+import os
+
+def create_info_message(yaml_file):
+    text = open(yaml_file,'r')
+    info = yaml.safe_load(text)
+    info_msg = CameraInfo()
+    genpy.message.fill_message_args(info_msg, info)
+    return info_msg
+
+def image_callback(msg, args):
+    camera_info_pub = args[0]
+    camera_info = args[1]
+    camera_info.header.stamp = msg.header.stamp
+    camera_info_pub.publish(camera_info)
+
+def process(unstamped_camera_info, camera_info_topic_name, image_topic_name):
+    rospy.init_node('camera_info_generator')
+    camera_info_pub = rospy.Publisher('camera_info_topic_name', String, queue_size=10)
+    rospy.Subscriber(image_topic_name, String, callback, (camera_info_pub, unstamped_camera_info))
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    if(len(argv) < 2):
+        print("ex: python p20_calibration.yaml /camera/image_raw")
+        quit()
+    yaml_file = argv[1]
+    image_topic_name = argv[2]
+    camera_info_topic_name = os.path.dirname(image_topic_name) + '/camera_info'
+    unstamped_camera_info = create_info_message(yaml_file)
+    process(unstamped_camera_info, camera_info_topic_name, image_topic_name)
