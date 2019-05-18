51706c5a51f86a34baadca9b026805c924248ab7
XiaoxingChen <chen-xx@zju.edu.cn>
2019-05-18 23:54:15 +0800

    Modify: acc from traj

--- acc_from_traj/acc_from_traj.py
+++ acc_from_traj/acc_from_traj.py
@@ -4,6 +4,9 @@ import numpy as np
 from sys import argv
 from tf.transformations import rotation_from_matrix
 from tf.transformations import quaternion_matrix
+import PlotCollection
+# import add_3axis_figure
+from add_3axis_figure import *
 
 
 def derivative(p, t):
@@ -16,50 +19,52 @@ def derivative(p, t):
     v = 1 / d2t * (dp[1:]*dt[:-1]/dt[1:] + dp[:-1]*dt[1:]/dt[:-1])
     return v
 
-def derivative3d(p3, t):
-    if(len(t) != len(p)):
-        print("length mismatch")
-        quit()
-    d2t = t[2:] - t[:-2]
-    dp = p[:][1:] - p[:][:-1]
-    dt = t[1:] - t[:-1]
-    v = 1 / d2t * (dp[1:]*dt[:-1]/dt[1:] + dp[:-1]*dt[1:]/dt[:-1])
-    return v
+def derivative3d(txyz):
+    dx_dt = derivative(txyz[1], txyz[0])
+    dy_dt = derivative(txyz[2], txyz[0])
+    dz_dt = derivative(txyz[3], txyz[0])
+    return np.array([txyz[0][1:-1], dx_dt, dy_dt, dz_dt])
 
-class Sequence4d():
-    def __init__(self):
-        self.t = np.array([])
-        self.x = np.array([])
-        self.y = np.array([])
-        self.z = np.array([])
-
-    def __init__(self, t, x, y, z):
-        self.t = np.array(t)
-        self.x = np.array(x)
-        self.y = np.array(y)
-        self.z = np.array(z)
-
-    def derivative(self):
-        dx_dt = derivative(self.x, self.t)
-        dy_dt = derivative(self.y, self.t)
-        dz_dt = derivative(self.z, self.t)
-        return Sequence4d(self.t[1:-1], dx_dt, dy_dt, dz_dt)
-    
-def Sequence4dFromTransformStamped(msgs):
+def PositionMatrixFromTransformStamped(msgs):
+    # txyz = [[msg.header.stamp.to_sec(), msg.transform.translation.x, msg.transform.translation.y, msg.transform.translation.z] for msg in msgs]
     t = [msg.header.stamp.to_sec()   for msg in msgs]
     x = [msg.transform.translation.x for msg in msgs]
     y = [msg.transform.translation.y for msg in msgs]
-    z = [msg.transform.translation.z for msg in msgs]    
-    return Sequence4d(t, x, y, z)
+    z = [msg.transform.translation.z for msg in msgs]  
+    
+    return np.array([t,x,y,z])
+
+def SE3FromTransformStamped(msgs):
+    t = [msg.header.stamp.to_sec()   for msg in msgs]
+    x = [msg.transform.translation.x for msg in msgs]
+    y = [msg.transform.translation.y for msg in msgs]
+    z = [msg.transform.translation.z for msg in msgs]  
+    qs = [msg.transform.rotation for msg in msgs]
+    r = [quaternion_matrix([q.x, q.y, q.z, q.w])[0:-1,0:-1] for q in qs]
+    return np.array([t,x,y,z,r])
+
+def ToBodyFrame(txyzr):
+    txyz = np.array(txyzr[0:-1,:])
+    for i in range(len(txyz[0])):
+        r = txyzr[4][i].transpose()
+        p = np.array(txyz[1:,i])
+        txyz[1:,i] = r.transpose().dot(p)
+    return txyz
 
 def RotationFromTransformStamped(msgs):
     t = [msg.header.stamp.to_sec() for msg in msgs]
     qs = [msg.pose.orientation for msg in msgs]
     r = [quaternion_matrix(rotation_from_matrix([q.x, q.y, q.z, q.w])) for q in qs]
     return (t, r)
-    
 
-def ReadTransformStamped(bag_filename, topic_name):
+def AccelerationFromIMU(msgs):
+    t = [msg.header.stamp.to_sec() for msg in msgs]
+    x = [msg.linear_acceleration.x for msg in msgs]
+    y = [msg.linear_acceleration.y for msg in msgs]
+    z = [msg.linear_acceleration.z for msg in msgs] 
+    return np.array([t,x,y,z])
+    
+def ReadTopicMsg(bag_filename, topic_name):
     msgs = []
     bag = rosbag.Bag(bag_filename)
     for topic, msg, t in bag.read_messages(topics=[topic_name]):
@@ -68,15 +73,36 @@ def ReadTransformStamped(bag_filename, topic_name):
 
 if __name__ == '__main__':
     if(len(argv) < 3):
-        print("Example: python acc_from_traj.py bag_path trajectory_topic_name")
+        print("Example: python acc_from_traj.py bag_path dataset_name")
         quit()
     bag_filename = argv[1]
-    topic_name = argv[2]
+    dataset_name = argv[2]
+    groundtruth_topic_name = ''
+    imu_topic_name = ''
+    if dataset_name == 'euroc':
+        groundtruth_topic_name = '/vicon/firefly_sbx/firefly_sbx'
+        imu_topic_name = '/imu0'
+    else:
+        print('unknown dataset')
+        quit()
 
-    msgs = ReadTransformStamped(bag_filename, topic_name)
-    positions = Sequence4dFromTransformStamped(msgs)
-    accs = positions.derivative().derivative()
-    print(len(positions.t))
-    print(len(accs.t))
+    transform_msgs = ReadTopicMsg(bag_filename, groundtruth_topic_name)
+    se3 = SE3FromTransformStamped(transform_msgs)
+    positions = ToBodyFrame(se3)
+    vels = derivative3d(positions)
+    accs = derivative3d(vels)
+    
+    imu_msgs = ReadTopicMsg(bag_filename, imu_topic_name)
+    acc_imu = AccelerationFromIMU(imu_msgs)
+
+    plotter = PlotCollection.PlotCollection("My window name")
+    pos = {'pos_b': positions, 'pos_w': se3[0:4]}
+    vel = {'vel': vels}
+    acc = {'acc': accs, 'acc_imu': acc_imu}
+    add_3axis_figure(plotter, "pos", pos)
+    add_3axis_figure(plotter, "vel", vel)
+    add_3axis_figure(plotter, "acc", acc)
+    plotter.show()
+
     
 
---
+++ plot_collection/add_3axis_figure.py
@@ -0,0 +1,41 @@
+import PlotCollection
+import pylab as pl
+
+def check_signal_dict(signal_dict):
+    for key in signal_dict:
+        for i in range(1,4):
+            if len(signal_dict[key][0]) != len(signal_dict[key][i]):
+                raise ValueError('signal {0} axis {1} length mismatch, len(t): {2}, len axis: {3}'.format(key, i, len(signal_dict[key][0]), len(signal_dict[key][i])))
+
+def add_3axis_figure(plotter, plot_name, vec3d_dict, linewidth=1):
+    fig = pl.figure()
+    check_signal_dict(vec3d_dict)
+    ax1 = fig.add_subplot(311)
+    for i in range(0, 3):
+        ax = None
+        if i == 0:
+            ax = ax1
+        else:
+            ax = fig.add_subplot(311 + i, sharex = ax1)
+        for key in vec3d_dict:
+            ax.plot(vec3d_dict[key][0], vec3d_dict[key][1+i], '-', label= key + str(i+1), linewidth=linewidth)
+            ax.set_ylabel(str(i+1))
+            # pl.setp(ax.get_xticklabels(), visible=False)
+            ax.legend()
+            ax.grid(1)
+    plotter.add_figure(plot_name, fig)
+        
+
+if __name__ == '__main__':
+    
+    sig = {'v': [[1,2,3],[0,1,1],[0,0,1],[1,1,2]]}
+
+    fig2=pl.figure()
+    pl.plot([3,1],[4,5])
+
+    plotter = PlotCollection.PlotCollection("My window name")
+    add_3axis_figure(plotter, "ddd", sig)
+    plotter.add_figure("My plot2 name", fig2)
+
+    #show collection
+    plotter.show()
