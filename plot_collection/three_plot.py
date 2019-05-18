a6245047fec7f2375809ad1403094cc6d3cf5503
XiaoxingChen <chen-xx@zju.edu.cn>
2019-05-18 17:46:13 +0800

    Modify: add_3axis_figure

--- plot_collection/three_plot.py
+++ plot_collection/three_plot.py
@@ -1,34 +1,28 @@
 import PlotCollection
 import pylab as pl
 
-#create the plot as usual
-fig1=pl.figure()
+def add_3axis_figure(plotter, plot_name, vec3d_dict, linewidth=1):
+    fig = pl.figure()
+    for i in range(0, 3):
+        ax = fig.add_subplot(311 + i)
+        for key in vec3d_dict:
+            ax.plot(vec3d_dict[key][0], vec3d_dict[key][1+i], label= key + str(i+1), linewidth=linewidth)
+            ax.set_ylabel(str(i+1))
+            pl.setp(ax.get_xticklabels(), visible=False)
+            ax.legend()
+    plotter.add_figure(plot_name, fig)
+        
 
-ax1 = fig1.add_subplot(311)
-ax1.plot([1,2,3], [4,5,6], 'r', label='ax', linewidth=1)
-ax1.set_ylabel('aaaa')
-pl.setp(ax1.get_xticklabels(), visible=False)
-ax1.legend()
+if __name__ == '__main__':
+    
+    sig = {'v': [[1,2,3],[0,1,1],[0,0,1],[1,1,2]]}
 
-ax2 = fig1.add_subplot(312, sharex=ax1)
-ax2.plot([1,2,3], [4,5,6], 'r', label='ax', linewidth=1)
-ax2.set_ylabel('bbbb')
-pl.setp(ax2.get_xticklabels(), visible=False)
-ax2.legend()
+    fig2=pl.figure()
+    pl.plot([3,1],[4,5])
 
-ax3 = fig1.add_subplot(313, sharex=ax1)
-ax3.plot([3,4,5], [4,5,6], 'r', label='ax', linewidth=1)
-ax3.set_ylabel('ccc')
-pl.setp(ax3.get_xticklabels(), visible=False)
-ax3.legend()
+    plotter = PlotCollection.PlotCollection("My window name")
+    add_3axis_figure(plotter, "ddd", sig)
+    plotter.add_figure("My plot2 name", fig2)
 
-fig2=pl.figure()
-pl.plot([3,1],[4,5])
-
-#add to collection
-plotter = PlotCollection.PlotCollection("My window name")
-plotter.add_figure("My plot1 name", fig1)
-plotter.add_figure("My plot2 name", fig2)
-
-#show collection
-plotter.show()
+    #show collection
+    plotter.show()
