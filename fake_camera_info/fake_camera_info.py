c2c98ceaccc016d908b6352242ec0f8eadf6b406
XiaoxingChen <chen-xx@zju.edu.cn>
2019-05-24 18:12:33 +0800

    Modify: fake camera info

--- fake_camera_info/fake_camera_info.py
+++ fake_camera_info/fake_camera_info.py
@@ -6,11 +6,26 @@ from sensor_msgs.msg import Image
 from sys import argv
 import os
 
+def format_calibration_to_msg(calib_format):
+    msg_format = {}
+    msg_format["header"] = {'seq': 0, 'stamp': {'secs': 0, 'nsecs': 0}, 'frame_id': ''}
+    msg_format["width"] = calib_format["image_width"]
+    msg_format["height"] = calib_format["image_height"]
+    msg_format["distortion_model"] = 'plumb_bob'
+    msg_format["D"] = calib_format['distortion_coefficients']['data']
+    msg_format["K"] = calib_format['camera_matrix']['data']
+    msg_format["R"] = calib_format['rectification_matrix']['data']
+    msg_format["P"] = calib_format['projection_matrix']['data']
+    msg_format["binning_x"] = 0
+    msg_format["binning_y"] = 0
+    msg_format["roi"] = {'x_offset': 0, 'y_offset': 0, 'height': 0, 'width': 0, 'do_rectify': False}
+    return msg_format
+
 def create_info_message(yaml_file):
     text = open(yaml_file,'r')
     info = yaml.safe_load(text)
     info_msg = CameraInfo()
-    genpy.message.fill_message_args(info_msg, info)
+    genpy.message.fill_message_args(info_msg, format_calibration_to_msg(info))
     return info_msg
 
 def image_callback(msg, args):
@@ -25,7 +40,6 @@ def process(unstamped_camera_info, camera_info_topic_name, image_topic_name):
     rospy.Subscriber(image_topic_name, Image, image_callback, (camera_info_pub, unstamped_camera_info))
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
-
 
 if __name__ == "__main__":
     # execute only if run as a script
