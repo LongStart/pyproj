1ae7b730f6b3e8b06a80028dd40c66a1979f0f28
XiaoxingChen <chen-xx@zju.edu.cn>
2019-07-07 22:59:07 +0800

    Modify: merge bags

--- ros_test/merge_bags.py
+++ ros_test/merge_bags.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
     if(len(argv) < 2):
         print('ex: python merge_bags.py output_name.bag in1.bag in2.bag ')
         quit()
-    output_bag_name = argv[1]
+    output_bag_name = 'merged_' + str(len(argv) - 1) + '.bag'
     output_bag = rosbag.Bag(output_bag_name, 'w')
-    for bag_name in argv[2:]:
+    for bag_name in argv[1:]:
         bag = rosbag.Bag(bag_name)
         for topic, msg, t in bag.read_messages():
             output_bag.write(topic, msg, t)
