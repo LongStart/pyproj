c2a4dc75b443f36d840d1180ec98f44a6c7a13e2
XiaoxingChen <chen-xx@zju.edu.cn>
2019-07-17 11:49:39 +0800

    Modify: don't save video

--- gif_for_rolling_shutter/matplotlib_animation.py
+++ gif_for_rolling_shutter/matplotlib_animation.py
@@ -5,10 +5,11 @@ from matplotlib.animation import FuncAnimation
 fig, ax = plt.subplots()
 xdata, ydata = [0, 10], [0, 10.]
 
-ln, = plt.plot([], [], '-', linewidth=10)
+ln, = plt.plot([], [], 'r-', linewidth=3)
 
 
 def init():
+    ax.grid(1)
     ax.set_xlim(0, 10.)
     ax.set_ylim(0, 10.)
     return ln,
@@ -20,7 +21,7 @@ def update(frame):
     ln.set_data(xdata, ydata)
     return ln,
 
-ani = FuncAnimation(fig, update, frames=np.hstack(5 * [np.linspace(0,10., 50)]) ,
-                    init_func=init, blit=True, interval = 0)
-ani.save('basic_animation.mp4', fps=60, extra_args=['-vcodec', 'libx264'])
+ani = FuncAnimation(fig, update, frames=np.linspace(0,10., 50) ,
+                    init_func=init, blit=True, interval = 1e-3)
+# ani.save('basic_animation.mp4', fps=60, extra_args=['-vcodec', 'libx264'])
 plt.show()
