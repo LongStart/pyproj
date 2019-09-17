9810ff170a4cfa0ee6b4c3b15153c01045bac387
XiaoxingChen <chen-xx@zju.edu.cn>
2019-09-17 18:01:28 +0800

    Add: test opencv chess board

---
+++ opencvtest/test_chess_board.py
@@ -0,0 +1,29 @@
+import cv2 as cv
+import sys
+import glob
+
+if __name__ == "__main__":
+    if(len(sys.argv) < 2):
+        print("Please input bag file name")
+        # print("Example: python {} path/*.pgm".format(sys.argv[0]))
+        quit()
+    imgs = sys.argv[1:]
+    board_size = (12,20)
+    for img_name in imgs:
+
+        img = cv.imread(img_name)
+
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        gray = cv.bitwise_not(gray)
+
+        flags = (cv.CALIB_CB_FAST_CHECK |
+                cv.CALIB_CB_ADAPTIVE_THRESH |
+                cv.CALIB_CB_FILTER_QUADS |
+                cv.CALIB_CB_NORMALIZE_IMAGE)
+        found, corners = cv.findChessboardCorners(gray, board_size, flags=flags)
+        print(found)
+        print(len(corners))
+        ret = 1
+        cv.drawChessboardCorners(img, board_size, corners, ret)
+        cv.imshow('asdf', img)
+        cv.waitKey(0)
